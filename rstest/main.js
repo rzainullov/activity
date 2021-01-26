5c0639663724587d509b63ef86a2c66318f8069b
rzainullov <rashid.zainullov@gmail.com>
2021-01-26 16:17:50 +0400

    fix:correct links

--- rstest/main.js
+++ rstest/main.js
@@ -1,12 +1,12 @@
 /* eslint-disable linebreak-style */
-import { Settings } from "../scripts/indexeddb/settings.js";
-import { loadSettingsFromLocalStorage } from "../scripts/indexeddb/settings.js";
-import { DataBase } from "../scripts/indexeddb/database.js";
-import { Login } from "../scripts/indexeddb/login.js";
-import { Modal } from "../scripts/modal/modal.js";
-import { loadSettingsFromDB } from "../scripts/indexeddb/settings.js";
-import { initModalLogin } from "../scripts/modal/modal-login.js";
-import { Audio } from "../scripts/audio.js";
+import { Settings } from "./scripts/indexeddb/settings.js";
+import { loadSettingsFromLocalStorage } from "./scripts/indexeddb/settings.js";
+import { DataBase } from "./scripts/indexeddb/database.js";
+import { Login } from "./scripts/indexeddb/login.js";
+import { Modal } from "./scripts/modal/modal.js";
+import { loadSettingsFromDB } from "./scripts/indexeddb/settings.js";
+import { initModalLogin } from "./scripts/modal/modal-login.js";
+import { Audio } from "./scripts/audio.js";
 
 export let currentLogin = new Login();
 export const globalSettings = new Settings(loadSettingsFromLocalStorage());
